78603ffa137b0f56b271267b634b8750e5cfb24d
newcrossing <newcrossing@gmail.com>
2024-08-19 22:04:33 +0300

    Бекап

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -19,7 +19,7 @@ class Kernel extends ConsoleKernel
 
         // бекап системы
         // $schedule->exec('/opt/php81/bin/php artisan backup:clean --disable-notifications')->dailyAt('21:03');
-        $schedule->exec("cd /var/www/localadmin/data/www/polrossii.ru/ && /opt/php81/bin/php artisan backup:run --disable-notifications")
+        $schedule->exec("cd  /var/www/localadmin/data/www/polrossii.ru/ && /opt/php81/bin/php artisan backup:run --disable-notifications")
             ->everyMinute()
             ->onFailure(function (Stringable $output) {
                 Log::warning('Бекап сайта завершился с ошибкой'.$output);
