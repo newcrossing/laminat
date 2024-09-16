6a4c1a1254e5f1fa9ecde9273447014b9c5f63f6
newcrossing <newcrossing@gmail.com>
2024-09-17 00:03:27 +0300

    Админка

---
+++ database/migrations/2024_09_16_234300_edit_products_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('products', function (Blueprint $table) {
+            $table->decimal('packing_volume')->after('square')->nullable()->default(null)->comment('объем упаковки');
+            $table->decimal('packing_weight')->after('square')->nullable()->default(null)->comment('вес упаковки');
+            $table->integer('number_of_boards')->after('square')->nullable()->default(null)->comment('досок в упаковке');;
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('products', function (Blueprint $table) {
+            $table->dropColumn(['packing_volume,packing_weight,number_of_boards']);
+        });
+    }
+};
