f9d4827e677fa28fcd1fbeb7a5ef501d4ce5130e
newcrossing <newcrossing@gmail.com>
2025-11-29 22:30:14 +0300

    images

--- app/Http/Controllers/Backend/PhotoController.php
+++ app/Http/Controllers/Backend/PhotoController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Backend;
 
 use App\Http\Controllers\Controller;
 use App\Models\Foto;
+use Exception;
 use Illuminate\Database\Eloquent\Relations\Relation;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
@@ -28,34 +29,41 @@ class PhotoController extends Controller
                 'extension' => $file->extension(),
             ]);
 
-            $image = ImageManager::imagick()->read($file);
-            if ($image->width() > $image->height()) {
-                $image->scale(height: 500)
-                    ->crop(400, 500, 0, 0, position: 'center-center')
-                    ->save(Storage::disk('product')->path('/cr_400/') . $foto->full_name_file);
+
+//            $image = ImageManager::imagick()->read($file);
+//            if ($image->width() > $image->height()) {
+//                $image->scale(height: 500)
+//                    ->crop(400, 500, 0, 0, position: 'center-center')
+//                    ->save(Storage::disk('product')->path('/cr_400/') . $foto->full_name_file);
+//                $image = ImageManager::imagick()->read($file);
+//                $image->scale(height: 125)
+//                    ->crop(100, 125, 0, 0, position: 'center-center')
+//                    ->save(Storage::disk('product')->path('/cr_100/') . $foto->full_name_file);
+//            } else {
+//                $image->scale(width: 400)
+//                    ->crop(400, 500, 0, 0, position: 'center-center')
+//                    ->save(Storage::disk('product')->path('/cr_400/') . $foto->full_name_file);
+//                $image = ImageManager::imagick()->read($file);
+//                $image->scale(width: 100)
+//                    ->crop(100, 125, 0, 0, position: 'center-center')
+//                    ->save(Storage::disk('product')->path('/cr_100/') . $foto->full_name_file);
+//            }
+
+
+//            $image = ImageManager::imagick()->read($file);
+//            $image->scale(width: 100)->save(Storage::disk('product')->path('/100/') . $foto->full_name_file);
+//            $image = ImageManager::imagick()->read($file);
+//            $image->scale(width: 300)->save(Storage::disk('product')->path('/300/') . $foto->full_name_file);
+//            $image = ImageManager::imagick()->read($file);
+//            $image->scale(width: 800)->save(Storage::disk('product')->path('/800/') . $foto->full_name_file);
+
+            try {
                 $image = ImageManager::imagick()->read($file);
-                $image->scale(height: 125)
-                    ->crop(100, 125, 0, 0, position: 'center-center')
-                    ->save(Storage::disk('product')->path('/cr_100/') . $foto->full_name_file);
-            } else {
-                $image->scale(width: 400)
-                    ->crop(400, 500, 0, 0, position: 'center-center')
-                    ->save(Storage::disk('product')->path('/cr_400/') . $foto->full_name_file);
-                $image = ImageManager::imagick()->read($file);
-                $image->scale(width: 100)
-                    ->crop(100, 125, 0, 0, position: 'center-center')
-                    ->save(Storage::disk('product')->path('/cr_100/') . $foto->full_name_file);
+                $image->scale(width: 1500)->save(Storage::disk('product')->path('/1500/') . $foto->full_name_file);
+            } catch (Exception $e) {
+                Log::error("PHP перехватил исключение:" . $e->getMessage());
             }
 
-
-            $image = ImageManager::imagick()->read($file);
-            $image->scale(width: 100)->save(Storage::disk('product')->path('/100/') . $foto->full_name_file);
-            $image = ImageManager::imagick()->read($file);
-            $image->scale(width: 300)->save(Storage::disk('product')->path('/300/') . $foto->full_name_file);
-            $image = ImageManager::imagick()->read($file);
-            $image->scale(width: 800)->save(Storage::disk('product')->path('/800/') . $foto->full_name_file);
-            $image = ImageManager::imagick()->read($file);
-            $image->scale(width: 1500)->save(Storage::disk('product')->path('/1500/') . $foto->full_name_file);
 
             $product = $model->find($request->id);
             $product->fotos()->save($foto);
