29597a545ce346d7394954edd67f8b345c9ee1dd
newcrossing <newcrossing@gmail.com>
2025-12-17 19:27:50 +0300

    бекап

--- config/backup.php
+++ config/backup.php
@@ -15,8 +15,8 @@ return [
                  * The list of directories and files that will be included in the backup.
                  */
                 'include' => [
-                    base_path('storage/app/public/'),
-                    base_path('public/storage/files/'),
+                    base_path('storage/app/public/files/'),
+                    base_path('storage/app/public/images/'),
                     base_path('public/storage/userfiles/'),
                 ],
 
